0bbb5bda288b02de5860919c3f50e713404b7bc7
rugbysafe <120204009+rugbysafe@users.noreply.github.com>
2022-12-30 17:32:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -62,39 +62,47 @@ stage6DateInput.addEventListener('focus', () => {
 
 injuryDateInput.addEventListener('keyup',({key}) => {
 	if (key === "Enter") {
+		calculateDates();
+	}
+});
+
+injuryDateInput.addEventListener('focusout',() => {
+	calculateDates();
+});
+
+function calculateDates() {
 		var txt = injuryDateInput.value;
 		var dt = new Date(txt);
 
 		var DateTime = luxon.DateTime;
-		var injuryDate = DateTime.fromFormat(txt,"dd/MM/yy")
-
+		var injuryDate = DateTime.fromFormat(txt,"dd/MM/yy");
+		
+		injuryDateInput.value = injuryDate.toFormat("dd/MM/yyyy");
+				
 		var stage1Date = injuryDate.plus({days:1});
-		stage1DateInput.value = stage1Date.toFormat("dd/MM/yy");
+		stage1DateInput.value = stage1Date.toFormat("dd/MM/yyyy");
 		
 		var stage2aDate = stage1Date.plus({days:2});
-		stage2aDateInput.value = stage2aDate.toFormat("dd/MM/yy");
+		stage2aDateInput.value = stage2aDate.toFormat("dd/MM/yyyy");
 	
 		//add the 48 hrs to the stage 2a date
 		var stage2bDate = stage1Date.plus({days:14});
-		stage2bDateInput.value = stage2bDate.toFormat("dd/MM/yy");
+		stage2bDateInput.value = stage2bDate.toFormat("dd/MM/yyyy");
 
 		//add the 48 hrs to the stage 2b date
 		var stage3Date = stage2bDate.plus({days:2});
-		stage3DateInput.value = stage3Date.toFormat("dd/MM/yy");
+		stage3DateInput.value = stage3Date.toFormat("dd/MM/yyyy");
 
 		//add the 48 hrs to the stage 3 date
 		var stage4Date = stage3Date.plus({days:2});
-		stage4DateInput.value = stage4Date.toFormat("dd/MM/yy");
+		stage4DateInput.value = stage4Date.toFormat("dd/MM/yyyy");
 		
 		//add the 48 hrs to the stage 4 date
 		var stage5Date = stage4Date.plus({days:2});
-		stage5DateInput.value = stage5Date.toFormat("dd/MM/yy");
+		stage5DateInput.value = stage5Date.toFormat("dd/MM/yyyy");
 
 		//add the 48 hrs to the stage 5 date
 		var stage6Date = stage5Date.plus({days:2});
-		stage6DateInput.value = stage6Date.toFormat("dd/MM/yy");
+		stage6DateInput.value = stage6Date.toFormat("dd/MM/yyyy");
 		
-		var returnDate = injuryDate.plus({days:23});
-		console.log(returnDate.toFormat("dd/MM/yy"));
-	}
-});
+};
